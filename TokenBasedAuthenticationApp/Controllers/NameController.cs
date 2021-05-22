29f86d5051a2e595deeca5b1e8630a2f3e555672
chakrapani24 <chakrapani24@outlook.com>
2021-05-22 19:33:24 +0530

    LBC1002 : Initial Commit

---
+++ TokenBasedAuthenticationApp/Controllers/NameController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TokenBasedAuthenticationApp.interfaces;
+using TokenBasedAuthenticationApp.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TokenBasedAuthenticationApp.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly IJwtAuthenticationManager jwtAuthenticationManager;
+        public NameController(IJwtAuthenticationManager jwtAuthenticationManager)
+        {
+            this.jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+        // GET: api/<NameController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token = jwtAuthenticationManager.Authenticate(userCred.UserName, userCred.Password);
+            if (token == null)
+                return Unauthorized();
+            return Ok(token);
+        }
+    }
+}
---
+++ TokenBasedAuthenticationApp/interfaces/IJwtAuthenticationManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TokenBasedAuthenticationApp.interfaces
+{
+    public interface IJwtAuthenticationManager
+    {
+        string Authenticate(string username, string password);
+    }
+}
